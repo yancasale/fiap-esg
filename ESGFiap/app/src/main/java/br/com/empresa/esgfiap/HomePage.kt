91c742acad33220c8dbb709be42b4c488bf7d88e
Yan Casale <79453924+yancasale@users.noreply.github.com>
2022-02-24 17:06:57 -0300

    Add files via upload

---
+++ ESGFiap/app/src/main/java/br/com/empresa/esgfiap/HomePage.kt
@@ -0,0 +1,11 @@
+package br.com.empresa.esgfiap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
